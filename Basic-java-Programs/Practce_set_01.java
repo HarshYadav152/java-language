6473e6eef88ba1d3a582c58b0325b332eac0a10e
harshyadav152 <harshyadav.personal01@gmail.com>
2025-02-12 21:25:57 +0530

    Loops

--- Basic-java-Programs/Practce_set_01.java
+++ Basic-java-Programs/Practce_set_01.java
@@ -1,3 +1,5 @@
+
+
 import java.util.Scanner;
 
 public class Practce_set_01 {
---
+++ Basic-java-Programs/break_and_continue.java
@@ -0,0 +1,24 @@
+class break_and_continue{
+    public static void main(String[] args) {
+        // break is used to exit from the running loop exiting remaining iteration
+
+        // for(int i = 0; i < 5; i++){
+        //     // loop runs when i's value is 3 it will terminate
+        //     if (i == 3) {
+        //         break; // break the loop
+        //     }
+        //     System.out.println(i);// print till 0 1 2 
+        // }
+
+        // continue skip the current iteration(including all next statement) and continue with next iteration
+
+        for (int i = 0; i < 10; i++) {
+            if (i == 5) {
+                continue; // skip the iteraion 
+            }
+            System.out.println(i); // it will print all values of i from 0 to 9 but not print 5
+        }
+
+        // both of these can be used in all loops
+    }
+}
